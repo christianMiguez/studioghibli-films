d61d13ee645c856366c2db5d22aef5d1fd815e2f
Tabaré Casalás <39859844+tabatulk@users.noreply.github.com>
2018-10-02 22:32:45 -0300

    new router app

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header />
+    <NavBar  />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+import Header from "@/components/Header.vue";
+export default {
+  name: 'App',
+  components: {
+    Header,
+    NavBar
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+  padding: 0px;
+  height: 100% !important;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="HeaderD1">
+        se esta cargando el header
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+
+}
+</script>
+
+<style>
+.Header {
+    
+}
+</style>
