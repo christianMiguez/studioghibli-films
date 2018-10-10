c2a27f0c64e4fd8ab245d1d955885d6fa0162675
Christian <kisquian@gmail.com>
2018-10-10 13:08:45 -0300

    nuevo rating, iconos y botones

---
+++ src/components/DetallePelicula.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="container2">
+        <div class="row textoDetalle" v-if="film">
+            <div>
+                
+                <router-link to="/" tag="button" class="btn"><font-awesome-icon icon="home" /> Back</router-link>
+
+            </div>
+            <h1>{{film.title}} </h1>
+            <div>
+                <h3>Description</h3>
+                <p>{{film.description}}</p>
+                <h3>Director</h3>
+                <p>{{film.director}}</p>
+                <h3>Producer</h3>
+                <p>{{film.producer}}</p>
+                <h3>Year</h3>
+                <p>{{film.release_date}}</p>
+                <h3>Rating</h3>
+                <p>{{film.rt_score}}</p>
+                <h3>Characters</h3>
+                <div class="peopleCards">
+                    <div v-for="people in people" :key="people.id">
+                        <div class="peopleCard" v-if="people.films[0].substr(38, 36) == film.id">
+                            <Cards v-bind:people="people" />
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+            
+        </div>
+        <div class="loadingElse" v-else>Loading...</div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Cards from "@/components/Cards.vue";
+  
+    export default {
+        data () {
+            return {
+                film: null,
+                people: null,
+                locations: null,
+            }
+        },
+        components: {
+            Cards
+        },
+        mounted () {
+            var idFilm = this.$route.params.idFilm
+            console.log('### ' + idFilm)
+            // cargar pelicula con id "idPelicula" desde la api
+            var comp = this
+            axios.get('https://ghibliapi.herokuapp.com/films/'+idFilm)
+            .then(function(res){
+                comp.film = res.data;
+            }, 
+            function(error){
+                console.log(error)
+            });
+            var apiUrl= 'https://ghibliapi.herokuapp.com/people/'
+            var apiPromise = axios.get('https://ghibliapi.herokuapp.com/people/')
+            .then(function(res){
+                comp.people = res.data
+            }, 
+            function(error){
+                console.log(error)
+            })
+            var apiUrl= 'https://ghibliapi.herokuapp.com/locations/'
+            var apiPromise = axios.get('https://ghibliapi.herokuapp.com/locations/')
+            .then(function(res){
+                comp.locations = res.data
+            }, 
+            function(error){
+                console.log(error)
+            })    
+            
+        }
+    }
+</script>
+
+<style scoped>
+h1 {
+    width: 100%;
+    padding-bottom:40px;
+    padding-top:10px;
+    font-weight:bold;
+    text-align: center
+
+}
+.container2 {
+    width: 1600px;
+    display: flex;
+    flex-direction: row;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: white;
+    margin-top: 10px;
+    margin-bottom: 10px;
+
+}
+.textoDetalle {
+        text-align: left;
+        margin: 15px;
+}
+.peopleCards {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content:flex-start;
+}
+.peopleCard {
+    margin-left: 10px;
+    width: min-content;
+}
+.loadingElse {
+    width: 100%;
+    text-align: center;
+    font-weight:bold;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStar, faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faStar, faHome)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+router.mode = 'history';
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
